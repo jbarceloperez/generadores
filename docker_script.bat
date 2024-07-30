d90ae5eadc44cfce413a9f83e9645bf27ac7ec6a
Javier Barceló <javibarpe@hotmail.com>
2024-07-30 02:23:01 +0200

    updated scripts and gui names

--- docker_script.bat
+++ docker_script.bat
@@ -2,7 +2,7 @@
 REM Comprueba si hay un argumento
 if "%1"=="" (
     echo Error: No se ha proporcionado ningun argumento.
-    echo Uso: script.bat [build | run | qtcreator]
+    echo Uso: script.bat [build | run | qtcreator | generador]
     exit /b 1
 )
 
@@ -18,11 +18,16 @@ if "%1"=="build" (
     echo Iniciando el contenedor y ejecutando Qt Creator...
     set DISPLAY=%DISPLAY%
     docker run -it --name qt_container --rm -e DISPLAY=host.docker.internal:0.0 -v /tmp/.X11-unix:/tmp/.X11-unix -v %cd%\Generadores:/workspace/Generadores -v %cd%\out:/workspace/out qtcreator6-6 qtcreator
+) else if "%1"=="generador" (
+    echo Iniciando el contenedor y ejecutando el generador de código...
+    set DISPLAY=%DISPLAY%
+    docker run -it --name qt_container --rm -e DISPLAY=host.docker.internal:0.0 -v /tmp/.X11-unix:/tmp/.X11-unix -v %cd%\Generadores:/workspace/Generadores -v %cd%\out:/workspace/out qtcreator6-6 bash -c "cd /workspace/Generadores/build-Generadores-Desktop-Debug && cmake .. && cmake --build . && ./codeGenerator"
 ) else (
-    echo Uso: %0 [build | run | qtcreator]
+    echo Uso: %0 [build | run | qtcreator | generador]
     echo   build      Construir la imagen Docker
     echo   run        Ejecutar el contenedor Docker
     echo   qtcreator  Iniciar el contenedor y ejecutar Qt Creator
+    echo   generador   Iniciar el contenedor y ejecutar el generador de código
     exit /b 1
 )
 
--- docker_script.sh
+++ docker_script.sh
@@ -6,10 +6,11 @@ CONTAINER_NAME="qt_container"
 
 # Función para mostrar el uso del script
 function show_usage() {
-    echo "Uso: $0 [build|run|qtcreator]"
-    echo "  build      Construir la imagen Docker"
-    echo "  run        Ejecutar el contenedor Docker"
-    echo "  qtcreator  Iniciar el contenedor y ejecutar Qt Creator"
+    echo "Uso: $0 [build | run | qtcreator | generador]"
+    echo "  build       Construir la imagen Docker"
+    echo "  run         Ejecutar el contenedor Docker"
+    echo "  qtcreator   Iniciar el contenedor y ejecutar Qt Creator"
+    echo "  generador   Iniciar el contenedor y ejecutar el generador de código"
 }
 
 # Función para construir la imagen Docker
@@ -22,15 +23,33 @@ function build_image() {
 function run_container() {
     echo "Ejecutando el contenedor Docker..."
     export DISPLAY=$DISPLAY
-    docker run -it --name $CONTAINER_NAME --rm -e DISPLAY=$DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix -v ./Generadores:/workspace/Generadores -v ./out:/workspace/out $IMAGE_NAME
+    docker run -it --name $CONTAINER_NAME --rm -e DISPLAY=$DISPLAY \
+    -v /tmp/.X11-unix:/tmp/.X11-unix \
+    -v ./Generadores:/workspace/Generadores \
+    -v ./out:/workspace/out $IMAGE_NAME
 }
 
 # Función para iniciar el contenedor y ejecutar Qt Creator
 function run_qtcreator() {
     echo "Iniciando el contenedor y ejecutando Qt Creator..."
     export DISPLAY=$DISPLAY
-    docker run -it --name $CONTAINER_NAME --rm -e DISPLAY=$DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix -v ./Generadores:/workspace/Generadores -v ./out:/workspace/out $IMAGE_NAME qtcreator
+    docker run -it --name $CONTAINER_NAME --rm -e DISPLAY=$DISPLAY -v\
+    -v /tmp/.X11-unix:/tmp/.X11-unix \
+    -v ./Generadores:/workspace/Generadores \
+    -v ./out:/workspace/out $IMAGE_NAME qtcreator
 }
+
+# Función para iniciar el contenedor y ejecutar el generador de código
+function run_codeGenerator() {
+    echo "Iniciando el contenedor y ejecutando el generador de código..."
+    export DISPLAY=$DISPLAY
+    docker run -it --name $CONTAINER_NAME --rm -e DISPLAY=$DISPLAY \
+    -v /tmp/.X11-unix:/tmp/.X11-unix \
+    -v ./Generadores:/workspace/Generadores \
+    -v ./out:/workspace/out \
+    $IMAGE_NAME bash -c "cd /workspace/Generadores/build-Generadores-Desktop-Debug && cmake .. && cmake --build . && ./codeGenerator"
+}
+
 
 # Verificar si se proporcionaron argumentos
 if [ $# -eq 0 ]; then
@@ -51,6 +70,10 @@ while [[ $# -gt 0 ]]; do
             ;;
         qtcreator)
             run_qtcreator
+            shift
+            ;;
+        generador)
+            run_codeGenerator
             shift
             ;;
         *)
